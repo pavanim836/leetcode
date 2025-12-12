8e7369255c7abdd5b02092cbaf86db59d776ef73
pavanim836 <pavanim836@gmail.com>
2025-12-12 23:22:05 +0530

    Update string.java

--- string.java
+++ string.java
@@ -29,6 +29,45 @@ class Solution {
         c = Character.toLowerCase(c);
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
-
-   
 }
+
+
+//<-------------reverse a string------->
+ class Solution {
+     public void reverseString(char[] s) {
+         int left = 0;
+         int right = s.length - 1;
+
+         while (left < right) {
+             char temp = s[left];
+             s[left] = s[right];
+             s[right] = temp;
+
+             left++;
+             right--;
+         }
+     }
+ }
+
+
+
+//<------------3330.find the original typed string------>
+ class Solution {
+     public int possibleStringCount(String word) {
+      
+         int n=1;
+         for(int i=0;i<word.length()-1;i++){
+        
+             if( word.charAt(i) == word.charAt(i+1) ){
+                 System.out.println(i);
+                 n++;
+      
+         
+           
+             }
+     }
+     return n;
+         }
+ }
+        
+  
