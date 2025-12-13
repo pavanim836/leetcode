29e6b062ce5615db4550c7708b0dff26d9c1e411
pavanim836 <pavanim836@gmail.com>
2025-12-13 21:20:53 +0530

    Update string.java

--- string.java
+++ string.java
@@ -69,5 +69,22 @@ class Solution {
      return n;
          }
  }
-        
-  
+
+//<--------------------125.valid palindrome--------------------->
+class Solution {
+    public boolean isPalindrome(String s) {
+       
+       s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            right--;
+            left++;
+        }
+        return true;
+    }
+}
+
