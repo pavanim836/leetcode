ba734b25dfc7a1685b3c7459ba8c14db7706f286
pavanim836 <pavanim836@gmail.com>
2025-12-14 20:03:39 +0530

    Update string.java

--- string.java
+++ string.java
@@ -88,3 +88,21 @@ class Solution {
     }
 }
 
+//<------------------58.length of last word---------------------->
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n=s.length()-1;
+        int count=0;
+        while(n>=0 && s.charAt(n)==' '){
+            n--;
+        }
+        while(n>=0 && s.charAt(n)!=' '){
+            count++;
+            n--;
+        }
+        return count;
+
+    }
+}
+  
+
