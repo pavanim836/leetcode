03f5b5318657c7c17f247ada055a7fa1f715a41e
pavanim836 <pavanim836@gmail.com>
2025-12-30 11:20:54 +0530

    deleted the array content
    Refactor methods for string manipulation to improve efficiency and readability.

--- string.java
+++ string.java
@@ -174,40 +174,6 @@ This is a key → value relationship.
 
 
 
-//<----------------------Count Indices to Balance Even and Odd Sums-------------------->
-class Solution {
-    public int cntWays(int[] arr) {
-        // code here
-        // ArrayList<Integer> indi = new ArrayList<Integer>();
-        int count=0;
-        int pos=0;
-         int n=arr.length;
-        for(int i=0;i<n;i++){
-            int sume=0;
-            int sumo=0;
-           
-        for(int j=0;j<n;j++){
-            
-            if(i==j){
-                continue;
-            }
-            if(pos%2==0){
-                sume=sume+arr[j];
-            }
-            else{
-                sumo=sumo+arr[j];
-            }
-            pos++;
-        }
-        
-            if(sume==sumo){
-                count++;
-            }
-        
-    }
-        return count;
-    }
-}
 
 
 
