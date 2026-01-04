8bd6a44e5c8257a8ed0ab081789016739422e537
pavanim836 <pavanim836@gmail.com>
2026-01-04 16:54:33 +0530

    Add plusOne method to increment digits array
    Implement plusOne method to increment an array representation of an integer.

--- array.java
+++ array.java
@@ -227,3 +227,26 @@ Final Output
         return new int[]{};
     }
 }
+<-------------------------------plus one------------------------------------->
+//Input: digits = [1,2,3]
+//Output: [1,2,4]
+//Explanation: The array represents the integer 123.
+//Incrementing by one gives 123 + 1 = 124.
+//Thus, the result should be [1,2,4].
+<--------------------------answer------------------------------>
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+         int[] result = new int[digits.length + 1];
+         result[0] = 1;
+         return result;
+    }
+}
+    
