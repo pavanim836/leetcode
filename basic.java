d66fa1971bd84126176f0aca4d33642716a1156c
pavanim836 <pavanim836@gmail.com>
2026-01-07 12:48:45 +0530

    Add palindrome number check in basic.java

---
+++ basic.java
@@ -0,0 +1,18 @@
+<-------------------------palindrome number------------------------------------>
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num=x;
+        int rev=0,rem;
+        while(x>0)
+        {
+            rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if(rev==num)
+        {
+            return true;
+        }
+        return false;
+    }
+}
