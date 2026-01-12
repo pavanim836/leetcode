fd9f5ea79d01f7a13382bb8d9074a1771e65d3bb
pavanim836 <pavanim836@gmail.com>
2026-01-12 22:12:45 +0530

    Refactor string manipulation methods in Solution class

--- string.java
+++ string.java
@@ -229,6 +229,39 @@ class Solution {
 //->Prefix "ab" has 2 distinct characters and length modulo 3 is 2, so it is a residue.
 //->Prefix "abc" does not satisfy the condition. Thus, the answer is 2.©leetcode
 
+<----------------------------roman to integer------------------------------------>
+    class Solution {
+    public int romanToInt(String s) {
+
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int curr = value(s.charAt(i));
+
+            // if next symbol exists and is bigger, subtract
+            if (i + 1 < s.length() && curr < value(s.charAt(i + 1))) {
+                total -= curr;
+            } else {
+                total += curr;
+            }
+        }
+
+        return total;
+    }
+
+    private int value(char c) {
+        if (c == 'I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        if (c == 'M') return 1000;
+        return 0;
+    }
+}
+
 
 
 
