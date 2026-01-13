d19e25b2db024fbf7ebe6268635eb54ec7b418b1
pavanim836 <pavanim836@gmail.com>
2026-01-13 23:32:24 +0530

    Add method to find kids with greatest candies
    Implemented a method to determine which kids can have the greatest number of candies after receiving extra candies.

--- array.java
+++ array.java
@@ -369,3 +369,34 @@ class Solution {
 //->The subarray [1, 0] has a sum of 1, which is present in the subarray.
 //->The subarray [-1, 1, 0] has a sum of 0, which is present in the subarray.
 //->Thus, the answer is 5.
+
+<--------------------------------kids with greatest number of candies------------------------------------>
+//Input: candies = [2,3,5,1,3], extraCandies = 3
+//Output: [true,true,true,false,true] 
+//Explanation: If you give all extraCandies to:
+//- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+//- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+//- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+//- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+//- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+<-----------------------------------answer------------------------------------>
+    class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> kwc=new ArrayList<>();
+        int max=candies[0];
+        for (int i=0;i<candies.length;i++){
+            if(candies[i]>max){
+                max=candies[i];
+            }
+        }
+         for (int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies>=max){
+                kwc.add(true);
+            }
+            else{
+                kwc.add(false);
+            }
+        }
+        return kwc;
+    }
+}
