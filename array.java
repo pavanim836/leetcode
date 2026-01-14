1878ad104b4f3f019d14096d82fa1ad7beeeafae
pavanim836 <pavanim836@gmail.com>
2026-01-14 23:27:00 +0530

    Add removeElement method to Solution class
    Implement method to remove specified element from array.

--- array.java
+++ array.java
@@ -400,3 +400,19 @@ class Solution {
         return kwc;
     }
 }
+<-------------------------------------remove element------------------------------------>
+//Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+//The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
