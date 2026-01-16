6b69033d1a499f89facbe2fcb098d016c49372ac
pavanim836 <pavanim836@gmail.com>
2026-01-16 23:11:32 +0530

    Add running sum method for 1-D array

--- array.java
+++ array.java
@@ -430,4 +430,13 @@ class Solution {
         return result;
     }
 }
+<---------------------------running sum of 1-D array------------------------------------------->
+    class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
 
