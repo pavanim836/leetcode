4dc428c69dedd4f3321445cff79f97078223e19d
pavanim836 <pavanim836@gmail.com>
2026-01-18 21:33:37 +0530

    Implement buildArray method in Solution class
    Added a method to build an array from a permutation of indices.

--- array.java
+++ array.java
@@ -440,3 +440,16 @@ class Solution {
     }
 }
 
+<-------------------build an array from permutation------------------------------------>
+    class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
