f0284c8af3b79fab7b3797fc46fabc732393ea97
pavanim836 <pavanim836@gmail.com>
2026-01-22 22:21:38 +0530

    Refactor string manipulation methods in Solution class

--- string.java
+++ string.java
@@ -315,6 +315,20 @@ class Solution {
 }
 
 
+<--------------------------find occurence of first index of a string--------------------------->
+    class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
 
 
 
