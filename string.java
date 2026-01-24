a246d0498361205adc19c43e13d2e8351d81fecc
pavanim836 <pavanim836@gmail.com>
2026-01-24 11:36:07 +0530

    Refactor string manipulation methods in Solution class

--- string.java
+++ string.java
@@ -351,6 +351,17 @@ class Solution {
     }
 }
 
+<----------------------------valid anagrams------------------------->
+    class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] x=s.toCharArray();
+        char[] y=t.toCharArray();
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+        return Arrays.equals(x,y);
+    }
+}
 
 
 
